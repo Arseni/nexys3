69a087e8e905f44dc7f8563ce451f6b4a3bbe8c2
Arseni <faber.dirk@gmail.com>
2012-06-11 19:58:00 +0200

    nix los

--- sseg_display.v
+++ sseg_display.v
@@ -23,19 +23,18 @@ module sseg_display
 	input rst,
 	input clk,
 	input wire [12:0] value,
-	output wire [3:0] sseg_an,
-	output wire [7:0] sseg_sig
+	output reg [3:0] sseg_an,
+	output reg [7:0] sseg_sig
 );
 
-wire dp;
-wire [3:0] digit [3:0];
-wire [7:0] sseg_sig_mux [3:0];
+reg [3:0] digit [3:0];
+reg [7:0] sseg_sig_mux [3:0];
 reg [1:0] sseg_sel;
 
-sseg sseg0 (digit[0], dp, sseg_sig_mux[0]);
-sseg sseg1 (digit[1], dp, sseg_sig_mux[1]);
-sseg sseg2 (digit[2], dp, sseg_sig_mux[2]);
-sseg sseg3 (digit[3], dp, sseg_sig_mux[3]);
+sseg sseg0 (digit[0], 'b1, sseg_sig_mux[0]);
+sseg sseg1 (digit[1], 'b0, sseg_sig_mux[1]);
+sseg sseg2 (digit[2], 'b0, sseg_sig_mux[2]);
+sseg sseg3 (digit[3], 'b0, sseg_sig_mux[3]);
 
 always @(posedge clk or rst)
 begin
@@ -45,16 +44,30 @@ begin
 		sseg_sel <= sseg_sel + 1;
 end
 
-assign digit[0] = value[7:4];
-assign digit[1] = value[3:0];
-
-assign dp = 1;
-
-assign sseg_sig =
-	sseg_sel == 'd0 ? sseg_sig_mux[0] :
-	sseg_sel == 'd1 ? sseg_sig_mux[1] :
-	sseg_sel == 'd2 ? sseg_sig_mux[2] :
-	sseg_sel == 'd3 ? sseg_sig_mux[3] :
-	'bZ;
+always @*
+begin
+	case (sseg_sel)
+	'd0: begin
+		sseg_sig = sseg_sig_mux[0];
+		sseg_an = 'b0001;
+	end
+	'd1: begin
+		sseg_sig = sseg_sig_mux[1];
+		sseg_an = 'b0010;
+	end
+	'd2: begin
+		sseg_sig = sseg_sig_mux[2];
+		sseg_an = 'b0100;
+	end
+	'd3: begin
+		sseg_sig = sseg_sig_mux[3];
+		sseg_an = 'b1000;
+	end
+	default: begin
+		sseg_sig = 'bZ;
+		sseg_an = 'bZ;
+	end
+	endcase
+end
 
 endmodule
